a26b046fb5594a58cf4a070b8e53f0c1ef46ed3a
taisei0211 <taisei2925@icloud.com>
2024-10-11 19:10:59 +0900

    Create oruga.c

---
+++ oruga.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("tomarunzyaneezo.......");
+    
+    int orugaituka;
+    orugaituka = 666;
+    
+    printf("Orugaituka is: %d",orugaituka);
+
+    return 0;
+}
