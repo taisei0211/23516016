22a8851d9e6ea1238af9206f57419f911ae3c549
taisei0211 <taisei2925@icloud.com>
2024-10-11 18:47:42 +0900

    Create github.c

---
+++ github.c
@@ -0,0 +1,14 @@
+iiassssssssssssssfhhhhhhhhhhhhhhhhhhhhhhsfhiuaeofehe
+eeehfre
+fewdrhuiwehaepdfa
+sfwergperg[erhgeroger
+gbher
+ger
+gfwer
+g-erg9-erhjgi0erjgfwergher
+  ^gi
+  er^ger\ger-9gjuear
+  gfear
+ger
+gaergbker^
+  gke5hg^w
