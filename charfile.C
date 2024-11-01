24aff712cef5b2a9bd88ce7b5f256d9cee32774b
taisei0211 <taisei2925@icloud.com>
2024-11-01 19:24:12 +0900

    Create charfile.C

---
+++ charfile.C
@@ -0,0 +1,12 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("char型の最大値:%d\n",CHAR_MAX);
+    printf("char型の最小値:%d\n",CHAR_MIN);
+    printf("uchar型の最大値:%d\n",UCHAR_MAX);
+    
+    return 0;
+}
